e0d45d392a55a89505f95e7eb326592976097d15
unknown <m.jahandide00@gmail.com>
2022-06-09 14:07:01 +0430

    design the search result screen and connect it to the provider

---
+++ lib/screens/search_result/components/search_list.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recipes/providers/search_provider.dart';
+import 'package:recipes/screens/components/error_screen.dart';
+import 'package:recipes/screens/components/list_and_error.dart';
+import 'package:recipes/screens/components/recipe_list.dart';
+import 'package:recipes/utils/data/recipe_data.dart';
+
+class SearchList extends StatelessWidget {
+  const SearchList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<SearchProvider>(
+      builder: ((BuildContext context, SearchProvider provider, child) {
+        if (provider.status != Error.noError) {
+          if (provider.recipes.isEmpty) {
+            return ErrorScreen(
+              error: provider.status,
+              onClick: provider.getData,
+            );
+          }
+          return ListAndError(
+            error: provider.status,
+            onClick: provider.getData,
+            list: RecipeList(
+              recipes: provider.recipes,
+              scrollController: provider.scrollController,
+            ),
+          );
+        } else if (provider.recipes.isEmpty) {
+          provider.getData();
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.greenAccent,
+            ),
+          );
+        }
+        return RecipeList(
+          recipes: provider.recipes,
+          scrollController: provider.scrollController,
+        );
+      }),
+    );
+  }
+}
--- lib/screens/search_result/search_result_screen.dart
+++ lib/screens/search_result/search_result_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipes/providers/search_provider.dart';
+import 'package:recipes/screens/components/search_box.dart';
+import 'package:recipes/screens/search_result/components/search_list.dart';
 
 class SearchResultScreen extends StatelessWidget {
   const SearchResultScreen({Key? key}) : super(key: key);
@@ -11,6 +13,14 @@ class SearchResultScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Result for '$query'"),
+      ),
+      body: Column(
+        children: const <Widget>[
+          SearchBox(),
+          Flexible(
+            child: SearchList(),
+          ),
+        ],
       ),
     );
   }
